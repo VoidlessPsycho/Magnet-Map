94fde799bb5ecd43ebf5ce328d957ababab54ca9
Voidless_Psycho <mgollapudi104@gmail.com>
2026-06-12 09:59:20 -0700

    Added Map, Sidebar

---
+++ countiesList.js
@@ -0,0 +1,14 @@
+export const countiesList = {
+    "Santa Cruz": {
+        places: {
+            name: "Mystery Spot",
+            img: "img/places/mystery-spot.png"
+        }
+    },
+    "Sonoma": {
+        places: {
+            name: "Peanuts Museum",
+            img: "img/places/peanuts-museum.png"
+        }
+    }
+}
--- scripts.js
+++ scripts.js
@@ -1,24 +1,38 @@
-const countiesList = {
-    "Santa Cruz" = {
-        places: ["Mystery Spot"],
-        img: "img/places/"
-    }
-}
+import { countiesList } from "countiesList.js";
+
 
 fetch("img/california.svg")
     .then(res => res.text())
     .then(svg => {
         document.getElementById("map").innerHTML = svg;
-        let counties = document.querySelectorAll('polyline[id^="_x"], path[id^="_x"]').forEach((county) => {
+        document.querySelectorAll('polyline[id^="_x"], path[id^="_x"]').forEach((county) => {
             let name = county.id.replace(/^_x\d+_\d+_/, "");
             name = name.replaceAll("_", " ");
             if (countiesList[name]) {
                 county.classList.add("visited");
             }
-            county.addEventListener("click", () => openCounty(county));
+            county.addEventListener("click", function () {
+                if (county.classList.contains("visited")) {
+                    openCounty(name);
+                }
+            });
         });
     });
 
 function openCounty(county) {
-    
+    let sidebar = document.getElementById("sidebar");
+    sidebar.innerHTML = "";
+
+    let countyName = document.createElement("h2");
+    countyName.textContent = county;
+    sidebar.append(countyName);
+
+    let placeName = document.createElement("h3");
+    placeName.textContent = countiesList[county].places.name;
+    sidebar.append(placeName);
+
+    let placeImg = document.createElement("img");
+    placeImg.setAttribute("src", countiesList[county].places.img);
+    sidebar.append(placeImg)
+
 }
